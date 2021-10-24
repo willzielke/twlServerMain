804ca6ee408ba8ce618488c05404439d59d93331
Will Zielke <willzielke@icloud.com>
2021-10-24 16:24:45 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace BlobQuickstartV12
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            string connectionString = "DefaultEndpointsProtocol=https;AccountName=twlazure;AccountKey=af2YhkWLvnOr6tXwxOiTFZdUzqGVsakV2g0G3BbGHxWcAPu/wCRJ+v1+xrAneoCtEV6QepmnCNrbI8AqBemOow==;EndpointSuffix=core.windows.net";
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            //Create a unique name for the container
+            string containerName = "quickstartblobs" + Guid.NewGuid().ToString();
+
+            // Create the container and return a container client object
+            BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+
+            // Create a local file in the ./data/ directory for uploading and downloading
+            string localPath = "./data/";
+            string fileName = "classRoomRequest" + Guid.NewGuid().ToString() + ".txt";
+            string localFilePath = Path.Combine(localPath, fileName);
+
+            // Write text to the file
+            await File.WriteAllTextAsync(localFilePath, "USE: example.com/example2.com/SPECIAL_PARAMETERS/etc.");
+
+            // Get a reference to a blob
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            Console.WriteLine("Uploading to Blob storage as blob:\n\t {0}\n", blobClient.Uri);
+
+            // Upload data from the local file
+            await blobClient.UploadAsync(localFilePath, true);
+
+            // Download the blob to a local file
+            // Append the string "DOWNLOADED" before the .txt extension 
+            // so you can compare the files in the data directory
+            string downloadFilePath = localFilePath.Replace(".txt", "DOWNLOADED.txt");
+
+            Console.WriteLine("\nDownloading blob to\n\t{0}\n", downloadFilePath);
+
+            // Download the blob's contents and save it to a file
+            await blobClient.DownloadToAsync(downloadFilePath);
+
+           /* // Clean up
+            Console.Write("Press any key to begin clean up");
+            Console.ReadLine();
+
+            Console.WriteLine("Deleting blob container...");
+            await containerClient.DeleteAsync();
+
+            Console.WriteLine("Deleting the local source and downloaded files...");
+            File.Delete(localFilePath);
+            File.Delete(downloadFilePath);
+
+            Console.WriteLine("Done");
+            */
+        }
+
+    }
+}
